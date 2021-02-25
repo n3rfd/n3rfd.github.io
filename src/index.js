2c9c69948127d5017c5929d71aee61d25ef867ef
Neftali Papelleras <50840971+n3rfd@users.noreply.github.com>
2021-02-25 09:28:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-#import Amplify from 'aws-amplify'
-#import config from './aws-exports'
-#Amplify.configure(config)
-
 ReactDOM.render(<App />, document.getElementById('root'));
